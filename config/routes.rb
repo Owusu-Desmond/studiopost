9b65ef0ccf957f1de6dc650cfb240c20fe24ff52
Owusu-Desmond <desmond0406114009@gmail.com>
2022-12-04 03:18:33 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   # root to: "home#index"
+
+  root to: "posts#index"
+
+  resources :users, only: [:new, :index] do
+    resources :posts, only: [:index, :show, :new, :destroy, :create] do
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
+    end
+  end
+
+  get 'users/:username', to: 'users#show', as: 'user'
 end
